54bf635842d9fbb32bae5fc3737ce407835eaf53
Hui Tang <duriantang@gmail.com>
2019-06-13 16:30:02 +0800

    import code from actix_redis

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate redis_async;
+#[macro_use]
+extern crate derive_more;
+
+mod redis;
+pub use redis::{Command, RedisActor};
+
+/// General purpose actix redis error
+#[derive(Debug, Display, From)]
+pub enum Error {
+    #[display(fmt = "Redis error {}", _0)]
+    Redis(redis_async::error::Error),
+    /// Receiving message during reconnecting
+    #[display(fmt = "Redis: Not connected")]
+    NotConnected,
+    /// Cancel all waters when connection get dropped
+    #[display(fmt = "Redis: Disconnected")]
+    Disconnected,
 }
+
+// re-export
+pub use redis_async::error::Error as RespError;
+pub use redis_async::resp::RespValue;
